cd7c3b26b3d5551df9800185aa290a7915b32b25
Gustavo Leon <gleon@magicleap.ds>
2022-11-04 13:07:03 -0400

    git commands

---
+++ git_commands.sh
@@ -0,0 +1,19 @@
+# this is ashell script but with work on windows too, 
+# pure git commands
+
+#to clone first time
+git clone <https of the repo>
+
+git clone https://github.com/gleon-magileap/vladihegu-gleon.git
+
+# then you get into the folder you just clone
+
+cd vladihegu-gleon
+
+#to get latest changes
+git pull
+
+# to puh changes
+git add .
+git commit -m "any message"
+git push 
